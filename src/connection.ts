093b16e6addd0acb8be1e58f9f538b3043e5d249
aggre <hiroyuki.aggre@gmail.com>
2022-08-25 16:17:31 +0900

    use the latest getConnection

--- src/connection.ts
+++ src/connection.ts
@@ -1,16 +1,5 @@
-/* eslint-disable functional/no-expression-statement */
 import { Connection, getConnection } from '@devprotocol/elements'
-import { define } from '@aggre/ullr'
 import { CONNECTION_ID } from '.'
 
-export const connection = (id: string = CONNECTION_ID): Connection => {
-	define(Connection)
-	const el =
-		getConnection(id) ??
-		((e) => {
-			e.setAttribute('id', id)
-			document.body.appendChild(e)
-			return e
-		})(document.createElement(Connection.is))
-	return el as Connection
-}
+export const connection = (id: string = CONNECTION_ID): Connection =>
+	getConnection(id)
