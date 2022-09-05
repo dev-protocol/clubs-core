6cf46bb4ea4a664c674af13bf20d18e7443173ca
aggre <hiroyuki.aggre@gmail.com>
2022-09-06 07:25:42 +0900

    exporting defineConfig throws unexpected error

--- src/astro.config.ts
+++ src/astro.config.ts
@@ -4,17 +4,5 @@ import lit from '@astrojs/lit'
 import react from '@astrojs/react'
 import svelte from '@astrojs/svelte'
 import vue from '@astrojs/vue'
-import { AstroUserConfig } from 'astro'
 
 export const integrations = [lit(), react(), svelte(), vue()]
-
-export const config = (overrides: AstroUserConfig = {}) =>
-	defineConfig(
-		mergeDeepRight({
-			server: {
-				port: 3000,
-			},
-			integrations,
-			overrides,
-		}) as AstroUserConfig
-	)
