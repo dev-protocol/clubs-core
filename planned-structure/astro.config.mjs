ed8f7eb45bf5776b37d4866afbd36015460a0ab1
aggre <hiroyuki.aggre@gmail.com>
2022-09-06 07:42:53 +0900

    remove unused/unsafe deps

--- planned-structure/astro.config.mjs
+++ planned-structure/astro.config.mjs
@@ -1,4 +1,7 @@
-import { integrations } from '../src/astro.config.mjs'
 import { defineConfig } from 'astro/config'
+import lit from '@astrojs/lit'
+import react from '@astrojs/react'
+import svelte from '@astrojs/svelte'
+import vue from '@astrojs/vue'
 
-export default defineConfig({ integrations })
+export default defineConfig({ integrations: [lit(), react(), svelte(), vue()] })
--- src/astro.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config'
-import { mergeDeepRight } from 'ramda'
-import lit from '@astrojs/lit'
-import react from '@astrojs/react'
-import svelte from '@astrojs/svelte'
-import vue from '@astrojs/vue'
-
-export const integrations = [lit(), react(), svelte(), vue()]
