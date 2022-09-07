307b78b12c9f202de8940388eae4f0c8c5d4fa1b
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2022-09-08 04:35:22 +0530

    ft: aside component (wip)

---
+++ src/components/Primitives/BaseButton.vue
@@ -0,0 +1,57 @@
+<template>
+	<button
+		class="text-sm font-medium px-12 py-4 rounded transition"
+		:class="classByStatus[status]"
+	>
+		<!-- Default Status -->
+		<slot v-if="status === 0" />
+		<!-- Loading Status -->
+		<p v-else-if="status === 1">
+			{{ loadingText }}
+		</p>
+		<!-- Success Status -->
+		<p v-else-if="status === 2">{{ successText }} &nbsp; &checkmark;</p>
+	</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'HSButton',
+	props: {
+		// Status: 0 = Default, 1 = Loading, 2 = Success
+		status: {
+			type: Number,
+			default: 0,
+		},
+		loadingText: {
+			type: String,
+			default: 'Loading',
+		},
+		successText: {
+			type: String,
+			default: 'Success',
+		},
+	},
+	data: () => ({
+		classByStatus: {
+			0: 'hs-button',
+			1: 'hs-button-loading',
+			2: 'hs-button-success',
+		},
+	}),
+})
+</script>
+
+<style scoped>
+.hs-button {
+	@apply bg-gray-900 hover:bg-gray-800 hover:ring-1 ring-gray-900;
+}
+.hs-button-loading {
+	@apply bg-gray-800 ring-2 ring-gray-900;
+}
+.hs-button-success {
+	@apply bg-green-600 ring-2 ring-green-700;
+}
+</style>
---
+++ src/layouts/Aside.vue
@@ -0,0 +1,36 @@
+<template>
+	<aside>
+		<BaseButton class="w-11/12">Connect Wallet</BaseButton>
+		<BaseButton
+			class="mt-6 w-11/12"
+			:status="0"
+			successText="Saved"
+			loadingText="Saving"
+			@click="save"
+			>Save</BaseButton
+		>
+	</aside>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import BaseButton from '../components/Primitives/BaseButton.vue'
+
+export default defineComponent({
+	name: 'Aside',
+	components: {
+		BaseButton,
+	},
+	props: ['clubs'],
+	data: () => ({
+		status: {
+			save: 0,
+		},
+	}),
+	methods: {
+		save() {
+			console.log('Save method called. Props to aside included', this.$props)
+		},
+	},
+})
+</script>
