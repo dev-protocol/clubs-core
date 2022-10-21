d540b4e4446a83d51d1baf835087cc1f6b09cf15
Matteu <matteugt@gmail.com>
2022-10-21 16:40:13 +0800

    FEAT: Add `CLBTier` in /react

---
+++ src/ui/react/CLBTier/CLBSkeleton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// @ts-ignore
+const CLBSkeleton: React.FC = () => {
+  return (
+		<div className="animate-pulse rounded bg-gray-500/60"></div>
+  );
+};
+
+export default CLBSkeleton;
