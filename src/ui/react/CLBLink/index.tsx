95d545aad3cdae8ed941c89adda14dd19966d389
Matteu <matteugt@gmail.com>
2022-10-21 16:39:35 +0800

    FEAT: Add `CLBLink` in /react

---
+++ src/ui/react/CLBLink/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface CLBLinkProps {
+	link: string;
+  children?: React.ReactNode|string;
+}
+
+// @ts-ignore
+const CLBLink: React.FC<CLBLinkProps> = ({link, children}) => {
+
+	const isLinkExternal: boolean = !!(link.startsWith('http://') || link.startsWith('https://'));
+
+  return (
+		<a href={link} className="hs-link" target={isLinkExternal ? '_blank' : '_self'}>
+			{children}
+		</a>
+  );
+};
+
+export default CLBLink;
