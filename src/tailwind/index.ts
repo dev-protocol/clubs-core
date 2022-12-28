ff05cce78dd3d748d83f5177882609258d869e67
aggre <hiroyuki.aggre@gmail.com>
2022-12-28 16:16:47 +0900

    add content to tailwind preset

--- src/tailwind/index.ts
+++ src/tailwind/index.ts
@@ -1,6 +1,9 @@
 import { theme } from '@devprotocol/hashi/tailwind'
 
 export const clubs = {
+	content: [
+		'./{src,node_modules/@devprotocol/clubs-core,node_modules/*clubs*plugin*}/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
+	],
 	theme: {
 		extend: {
 			screens: theme.screens,
