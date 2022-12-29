78144bd70079abee57e83e2d871003a908a4308e
aggre <hiroyuki.aggre@gmail.com>
2022-12-29 21:36:28 +0900

    Revert "impl new integrations api"
    This reverts commit 9ad3e8493b7fd8eaf18cd0344dffcb5a78857cde.

--- src/integrations.ts
+++ src/integrations.ts
@@ -1,44 +1,22 @@
 /* eslint-disable functional/no-expression-statement */
 import type { AstroIntegration } from 'astro'
-import tailwind from '@astrojs/tailwind'
-import svelte from '@astrojs/svelte'
-import vue from '@astrojs/vue'
-import react from '@astrojs/react'
-import prefetch from '@astrojs/prefetch'
-import markdownIntegration from '@astropub/md'
 import { ClubsAstroIntegrationOptions } from './types'
 
 export const astro = function (
 	options?: ClubsAstroIntegrationOptions
-): readonly AstroIntegration[] {
-	return [
-		{
-			name: '@devprotocol/clubs-core',
-			hooks: {
-				'astro:config:setup': async ({ updateConfig }) => {
-					updateConfig({
-						vite: {
-							ssr: {
-								noExternal: [/^.*clubs-plugin.*$/],
-							},
+): AstroIntegration {
+	return {
+		name: '@devprotocol/clubs-core',
+		hooks: {
+			'astro:config:setup': async ({ updateConfig }) => {
+				updateConfig({
+					vite: {
+						ssr: {
+							noExternal: [/^.*clubs-plugin.*$/],
 						},
-					})
-				},
+					},
+				})
 			},
 		},
-		markdownIntegration(),
-		tailwind(),
-		svelte(),
-		vue({
-			template: {
-				compilerOptions: {
-					isCustomElement: (tag) => tag.includes('-'),
-				},
-			},
-		}),
-		react(),
-		prefetch({
-			throttle: 10,
-		}),
-	]
+	}
 }
