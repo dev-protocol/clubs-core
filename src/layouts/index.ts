79c155ed1e50106df1f9b63b39ee35169b2f2a0f
aggre <hiroyuki.aggre@gmail.com>
2023-01-24 01:38:15 +0900

    add export Aside.astro

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,2 +1,3 @@
 export { default as Admin } from './Admin.astro'
 export { default as AdminConfigurationManager } from './AdminConfigurationManager.astro'
+export { default as Aside } from './Aside.astro'
