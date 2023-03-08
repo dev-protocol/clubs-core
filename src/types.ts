28d708d7ef4a3ceca6df0db31f020f0b0e538158
aggre <hiroyuki.aggre@gmail.com>
2023-03-08 12:28:11 +0900

    add new types: ClubsPluginTokens and ClubsPluginOffers

--- src/types.ts
+++ src/types.ts
@@ -150,10 +150,33 @@ export enum ClubsPluginSignal {
 	DisplayFullPage = 'display:full-page',
 }
 
+export type ClubsPluginTokens = Omit<
+	ClubsConfiguration,
+	| 'name'
+	| 'twitterHandle'
+	| 'description'
+	| 'url'
+	| 'adminRolePoints'
+	| 'rpcUrl'
+	| 'options'
+	| 'plugins'
+>
+
+// Ref.: https://schema.org/Offer
+export type ClubsPluginOffers = {
+	readonly price?: 0
+	readonly priceCurrency?: 'ETH' | 'DEV'
+	readonly requiredSTokens?: {
+		readonly payload?: Uint8Array | string
+	}
+}
+
 export type ClubsPluginMeta = {
 	readonly id?: string
 	readonly displayName: string
 	readonly category: ClubsPluginCategory | string
+	readonly tokens?: ClubsPluginTokens
+	readonly offers?: ClubsPluginOffers
 }
 
 export type ClubsThemePluginMeta = ClubsPluginMeta & {
