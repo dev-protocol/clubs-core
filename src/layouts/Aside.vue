a9ba52331ca24a2fc62f9d3032531a5cf98359de
aggre <hiroyuki.aggre@gmail.com>
2023-03-29 16:37:25 +0900

    display erorr message

--- src/layouts/Aside.vue
+++ src/layouts/Aside.vue
@@ -5,14 +5,22 @@
 			v-if="showConnectButton"
 			class="w-full"
 		/>
-		<ActionButton
-			class="w-full"
-			:status="status.save"
-			successText="Saved"
-			loadingText="Saving"
-			@click="save"
-			>Save</ActionButton
-		>
+		<div class="relative">
+			<ActionButton
+				class="w-full"
+				:status="status.save"
+				successText="Saved"
+				loadingText="Saving"
+				@click="save"
+				>Save</ActionButton
+			>
+			<div
+				v-if="status.error"
+				class="bg-danger-300 mx-auto -mt-4 max-w-[99%] rounded px-4 pt-6 pb-2 text-sm text-white shadow"
+			>
+				{{ status.error }}
+			</div>
+		</div>
 		<slot></slot>
 	</aside>
 </template>
@@ -47,6 +55,7 @@ export default defineComponent({
 	data: () => ({
 		status: {
 			save: 0,
+			error: null as ReturnType<typeof Error> | null,
 		},
 		timeout: null as ReturnType<typeof setTimeout> | null,
 	}),
@@ -61,11 +70,13 @@ export default defineComponent({
 					if (typeof ev.detail.success === 'boolean') {
 						if (ev.detail.success) {
 							this.status.save = 2 // Success state
+							this.status.error = null
 
 							// Reset save button to default state after showing success for 3 seconds
 							timer = setTimeout(() => (this.status.save = 0), 3000)
 						} else {
 							this.status.save = 3 // Error state
+							this.status.error = ev.detail.error
 
 							// Reset save button to default state after showing error for 3 seconds
 							timer = setTimeout(() => (this.status.save = 0), 3000)
