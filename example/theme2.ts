5dba490970241815a706af41a8fb5e5a2ed97432
aggre <hiroyuki.aggre@gmail.com>
2023-04-05 00:19:06 +0900

    add example implementation of theme switcher

---
+++ example/theme2.ts
@@ -0,0 +1,46 @@
+/* eslint-disable functional/functional-parameters */
+import { default as Layout } from './Layout.astro'
+import { default as Admin } from './Admin.astro'
+import { default as Modal } from './Modal.astro'
+import {
+	ClubsFunctionGetPagePaths,
+	ClubsFunctionGetAdminPaths,
+	ClubsFunctionGetSlots,
+	ClubsFunctionGetLayout,
+	ClubsSlotName,
+} from '../src'
+
+export const getPagePaths: ClubsFunctionGetPagePaths = async () => []
+
+export const getAdminPaths: ClubsFunctionGetAdminPaths = async () => [
+	{ paths: ['theme'], component: Admin },
+]
+
+export const getLayout: ClubsFunctionGetLayout = async () => ({
+	layout: Layout,
+})
+
+export const getSlots: ClubsFunctionGetSlots = async (
+	options,
+	_,
+	{ paths }
+) => {
+	return [
+		{
+			slot: ClubsSlotName.AdminModalCcontent,
+			component: Modal,
+			props: { x: 456 },
+		},
+	]
+}
+
+export const meta = {
+	id: 'theme2',
+	displayName: 'Example 2',
+	category: 'theme',
+	theme: {
+		previewImage: 'https://dummyimage.com/600x400/2fc495/000000',
+	},
+}
+
+export default { getPagePaths, getAdminPaths, getLayout, getSlots, meta }
