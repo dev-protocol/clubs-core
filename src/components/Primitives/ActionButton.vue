da7b11e099d4d75b35a60327554ca7350884efc9
Matteu <matteugt@gmail.com>
2023-05-09 18:54:54 +0800

    UPDATE: Improve ActionButton component

--- src/components/Primitives/ActionButton.vue
+++ src/components/Primitives/ActionButton.vue
@@ -1,59 +1,59 @@
 <template>
-  <button
-	class="hs-button is-large is-filled transition"
-	:class="classByStatus[status]"
-  >
-	<!-- Default Status -->
-	<slot v-if="status === 0"/>
-	<!-- Loading Status -->
-	<div
-	  v-else-if="status === 1"
-	  class="flex items-center justify-center space-x-2 rounded-lg"
+	<button
+		class="hs-button is-large is-filled transition"
+		:class="classByStatus[status]"
 	>
-	  <span class="text-sm font-medium">{{ loadingText }}</span>
-	  <Spinner class="mt-0.5"/>
-	</div>
-	<!-- Success Status -->
-	<template v-else-if="status === 2">{{ successText }} &checkmark;</template>
-	<!-- Error Status -->
-	<template v-else-if="status === 3">{{ errorText }} &cross;</template>
-  </button>
+		<!-- Default Status -->
+		<slot v-if="status === 0" />
+		<!-- Loading Status -->
+		<div
+			v-else-if="status === 1"
+			class="flex items-center justify-center space-x-2 rounded-lg"
+		>
+			<span class="text-sm font-medium">{{ loadingText }}</span>
+			<Spinner class="mt-0.5" />
+		</div>
+		<!-- Success Status -->
+		<template v-else-if="status === 2">{{ successText }} &checkmark;</template>
+		<!-- Error Status -->
+		<template v-else-if="status === 3">{{ errorText }} &cross;</template>
+	</button>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue'
+import { defineComponent } from 'vue'
 import Spinner from '../Icons/Spinner.vue'
 
 export default defineComponent({
-  name: 'BaseButton',
-  props: {
-	// Status: 0 = Default, 1 = Loading, 2 = Success
-	status: {
-	  type: Number,
-	  default: 0,
+	name: 'BaseButton',
+	props: {
+		// Status: 0 = Default, 1 = Loading, 2 = Success
+		status: {
+			type: Number,
+			default: 0,
+		},
+		loadingText: {
+			type: String,
+			default: 'Loading',
+		},
+		successText: {
+			type: String,
+			default: 'Success',
+		},
+		errorText: {
+			type: String,
+			default: 'Error',
+		},
 	},
-	loadingText: {
-	  type: String,
-	  default: 'Loading',
-	},
-	successText: {
-	  type: String,
-	  default: 'Success',
-	},
-	errorText: {
-	  type: String,
-	  default: 'Error',
-	},
-  },
-  data: () => ({
-	classByStatus: {
-	  0: 'is-action-button',
-	  1: 'is-loading',
-	  2: 'is-success',
-	  3: 'is-error',
-	},
-  }),
-  components: {Spinner},
+	data: () => ({
+		classByStatus: {
+			0: 'is-action-button',
+			1: 'is-loading',
+			2: 'is-success',
+			3: 'is-error',
+		},
+	}),
+	components: { Spinner },
 })
 </script>
 
@@ -77,14 +77,16 @@ export default defineComponent({
 // }
 
 .action-button-loading {
-  @apply border-2 border-gray-900 bg-gray-800;
+	@apply border-2 border-gray-900 bg-gray-800;
 }
 
 @include hs-button.extend('success') {
-  @include hs-button.color((
-	fill: 'dp-green.400',
-	ink: 'dp-green.ink',
-	border: 'dp-green.400'
-  ));
+	@include hs-button.color(
+		(
+			fill: 'dp-green.400',
+			ink: 'dp-green.ink',
+			border: 'dp-green.400',
+		)
+	);
 }
 </style>
