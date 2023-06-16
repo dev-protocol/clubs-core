cc7f0cc37ccca10f90874ffb1f5e99cb890453c5
Matteu <matteugt@gmail.com>
2023-06-17 03:03:51 +0800

    FEAT: Add `clb-button-group` component

---
+++ src/ui/vue/CLBButtonGroup.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="hs-button-group">
+		<slot />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'CLBButtonGroup'
+}
+</script>
---
+++ src/ui/vue/CLBButtonGroupItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <input :id="id" type="radio" :name="name" :value="value" :checked="isChecked" :disabled="isDisabled">
+  <label :for="id" :class="`hs-button is-outlined${isDisabled ? ' is-disabled' : ''}`">
+  	<span class="hs-button__label"><slot /></span>
+	</label>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'CLBButtonGroupItem',
+	props: {
+		id: String,
+		name: String,
+		value: String,
+		isChecked: Boolean,
+		isDisabled: Boolean,
+	}
+}
+</script>
