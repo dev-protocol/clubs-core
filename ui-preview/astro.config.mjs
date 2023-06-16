b6fa521bb4d6dc6f9744cfd8d5d1acdb522941dc
Matteu <matteugt@gmail.com>
2023-06-17 03:01:10 +0800

    FEAT: Add svelte to UI preview

--- ui-preview/astro.config.mjs
+++ ui-preview/astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import vue from '@astrojs/vue'
+import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
 	server: {
 		port: 3000,
 	},
-	integrations: [vue(), react(), tailwind()],
+	integrations: [vue(), react(), svelte(), tailwind()],
 })
