3918a439c80feb98b3389134160506d1d56bef2f
Matteu <matteugt@gmail.com>
2023-06-17 03:04:53 +0800

    UPDATE: Update export paths

--- src/ui/react/index.ts
+++ src/ui/react/index.ts
@@ -1,19 +1,20 @@
 import CLBButton from './CLBButton'
+import {CLBButtonGroup, CLBButtonGroupItem} from './CLBButtonGroup'
 import CLBCheckbox from './CLBCheckbox'
 import CLBFormField from './CLBFormField'
 import CLBLink from './CLBLink'
 import CLBRadio from './CLBRadio'
 import CLBSelectField from './CLBSelectField'
 import CLBTier from './CLBTier'
-import CLBWrapper from './CLBWrapper'
 
 export {
 	CLBButton,
+	CLBButtonGroup,
+	CLBButtonGroupItem,
 	CLBCheckbox,
 	CLBFormField,
 	CLBLink,
 	CLBRadio,
 	CLBSelectField,
 	CLBTier,
-	CLBWrapper,
 }
