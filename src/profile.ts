6fc81fd9da8c17ac4667975f5ca10d94b8f7d94f
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-08-09 16:50:24 +0900

    fetch profile util

---
+++ src/profile.ts
@@ -0,0 +1,46 @@
+import { isAddress } from 'ethers'
+
+/**
+ * The profile of a user
+ */
+export type Profile = {
+	readonly avatar: string
+	readonly username: string
+}
+
+/**
+ * Fetches the profile of the user
+ *
+ * @param {string} address - The wallet adddress of the user
+ *
+ * @example
+ *
+ * ```ts
+ * import { fetchProfile } from '@devprotocol/clubs-core'
+ *
+ * const { profile, error } = await fetchProfile('0x...')
+ * ```
+ */
+export const fetchProfile = async (
+	address: string
+): Promise<{
+	readonly profile: Profile | undefined
+	readonly error: Error | undefined
+}> => {
+	// eslint-disable-next-line functional/no-conditional-statement
+	if (!isAddress(address)) {
+		return {
+			profile: undefined,
+			error: new Error('Invalid address'),
+		}
+	}
+
+	const apiEndpoint = `https://clubs.place/api/profile/${address}`
+
+	const res = await fetch(apiEndpoint)
+	const profile = (await res.json()) as Profile
+	return {
+		profile,
+		error: undefined,
+	}
+}
