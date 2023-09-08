5273889c6a470e2324804b6362af409f51c3931a
aggre <hiroyuki.aggre@gmail.com>
2023-09-08 23:26:07 +0900

    use @web3modal/wagmi

---
+++ src/components/Connect.vue
@@ -0,0 +1,76 @@
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/vue'
+import { mainnet, polygon, polygonMumbai } from '@wagmi/core/chains'
+import { useWeb3Modal } from '@web3modal/wagmi/vue'
+import { getWalletClient } from '@wagmi/core'
+import { whenDefined } from '@devprotocol/util-ts'
+import { BrowserProvider } from 'ethers'
+
+const props = defineProps<{
+	projectId: string
+	label?: string
+	class?: string
+	overrideClass?: string
+	chainId?: number
+}>()
+
+const truncatedAddress = ref<string>()
+const truncateAddress = (address: string) => {
+	const match = address.match(
+		/^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/
+	)
+	return !match ? address : `${match[1]}\u2026${match[2]}`
+}
+
+const chains = [
+	props.chainId === 137
+		? polygon
+		: props.chainId === 80001
+		? polygonMumbai
+		: props.chainId === 1
+		? mainnet
+		: polygon,
+]
+const wagmiConfig = defaultWagmiConfig({
+	chains,
+	projectId: props.projectId,
+	appName: 'Web3Modal',
+})
+
+createWeb3Modal({ wagmiConfig, projectId: props.projectId, chains })
+
+const modal = useWeb3Modal()
+
+onMounted(async () => {
+	const { connection } = await import('../connection')
+	whenDefined(await getWalletClient(), (wallet) =>
+		connection().setEip1193Provider(wallet.transport, BrowserProvider)
+	)
+	connection().account.subscribe((account) => {
+		truncatedAddress.value = whenDefined(account, (a) => truncateAddress(a))
+	})
+})
+</script>
+
+<template>
+	<button
+		:class="
+			props.overrideClass
+				? props.overrideClass
+				: 'hs-button is-filled is-large is-fullwidth data-[is-loading=true]:animate-pulse'
+		"
+		v-bind:class="props.class"
+		@click="modal.open()"
+	>
+		<span className="hs-button__label">
+			{{
+				truncatedAddress
+					? truncatedAddress
+					: props.label
+					? props.label
+					: 'Connect'
+			}}
+		</span>
+	</button>
+</template>
