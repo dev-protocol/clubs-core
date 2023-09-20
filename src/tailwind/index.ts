3cc14f16f8e463830f2484bc0cbadc59d46e0d3f
aggre <hiroyuki.aggre@gmail.com>
2023-09-20 20:35:51 +0900

    bugfix for tailwind preset

--- src/tailwind/index.ts
+++ src/tailwind/index.ts
@@ -6,7 +6,7 @@ import { hsExtendedColorTokens } from '@devprotocol/hashi/tailwind/extensions'
 
 export const clubs = {
 	content: [
-		'./{src,node_modules/@devprotocol/clubs-core,node_modules/*clubs*plugin*}/**/*.{astro,svelte,vue,html,js,jsx,ts,tsx}',
+		'./{src,node_modules/@devprotocol/clubs-core,node_modules/**/*clubs*plugin*}/**/*.{astro,svelte,vue,html,js,jsx,ts,tsx}',
 	],
 	theme: {
 		extend: {
