730fcb16e3758311602c1a15bdc79f95dac987a1
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-10-18 14:21:45 +0900

    linting

--- src/bytes32Hex.test.ts
+++ src/bytes32Hex.test.ts
@@ -8,7 +8,6 @@ import { bytes32Hex } from './bytes32Hex'
  * @todo Implement this test
  */
 test('Passing a UInt8Array returns keccak256(val)', (t) => {
-
 	const str = 'test'
 	// change to Uint8Array
 	const uint8Array = new Uint8Array(str.length)
@@ -17,11 +16,13 @@ test('Passing a UInt8Array returns keccak256(val)', (t) => {
 	}
 
 	const res = bytes32Hex(uint8Array)
-	t.deepEqual(res, '0x9c22ff5f21f0b81b113e63f7db6da94fedef11b2119b4088b89664fb9a3cb658')
+	t.deepEqual(
+		res,
+		'0x9c22ff5f21f0b81b113e63f7db6da94fedef11b2119b4088b89664fb9a3cb658'
+	)
 })
 
 test('Passing a string returns itself', (t) => {
-
 	const str = 'test'
 
 	const res = bytes32Hex(str)
