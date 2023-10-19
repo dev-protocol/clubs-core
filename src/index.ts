af7ea10eb5acf56a5bbec29a316b737911d076d8
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-10-19 11:10:50 +0900

    exports functions from events

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,19 @@ export { decode } from './decode'
 export { encode } from './encode'
 export { bytes32Hex } from './bytes32Hex'
 export { pageFactory, adminFactory, apiFactory } from './factory'
-export { setOptions, setConfig, onSubmitConfig } from './events'
+export {
+	setOptions,
+	setConfig,
+	onSubmitConfig,
+	buildConfig,
+	onMountClient,
+	controlModal,
+	onFinishConfig,
+	onUpdatedConfiguration,
+	submitConfig,
+	updatedConfig,
+	updatedOptions,
+} from './events'
 export { toBytes } from './parser'
 export { authenticate } from './authenticate'
 export { clubs as tailwindPreset } from './tailwind'
