fea9bccd775bf0df4ed419173fe51e997015640b
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-10-19 10:48:45 +0900

    removes tailwind import

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,7 +8,6 @@ const majorCoreAPIs = [
 	'dist/src/layouts/index.js',
 	'dist/src/connection/index.js',
 	'dist/src/styles/index.js',
-	'dist/src/tailwind/index.js',
 ]
 
 const useSrc = ({ out, ext } = {}) => ({
