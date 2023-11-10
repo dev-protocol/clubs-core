e94cb39d7b0c59e6c6d905b4973c6a6ef25ea45c
aggre <hiroyuki.aggre@gmail.com>
2023-11-10 15:11:42 +0900

    typing improvements

--- src/ui/components/Checkout/types.ts
+++ src/ui/components/Checkout/types.ts
@@ -1,4 +1,5 @@
 import type { CurrencyOption } from '../../../constants/currencyOption'
+import type { ClubsPropsPages } from '../../../types'
 
 export type CheckoutOptions = {
 	readonly amount: number
@@ -13,4 +14,19 @@ export type CheckoutOptions = {
 	readonly feePercentage?: number
 	readonly payload?: Uint8Array | string
 	readonly description?: string
+	readonly useDiscretePaymentFlow?: boolean
+	readonly useInjectedTransactionForm?: boolean
+	readonly itemImageSrc?: string
+	readonly itemName?: string
+	readonly accessControlUrl?: string
+	readonly accessControlDescription?: string
 }
+
+export type ResultOptions = {
+	readonly id?: string | number
+	readonly rpcUrl: string
+}
+
+export type CheckoutProps = ClubsPropsPages & CheckoutOptions
+
+export type ResultProps = ClubsPropsPages & ResultOptions
--- src/ui/components/index.ts
+++ src/ui/components/index.ts
@@ -1,3 +1,8 @@
 export { default as Checkout } from './Checkout/Checkout.astro'
 export { default as Result } from './Checkout/Result.astro'
-export type { CheckoutOptions } from './Checkout/types'
+export type {
+	CheckoutOptions,
+	CheckoutProps,
+	ResultOptions,
+	ResultProps,
+} from './Checkout/types'
