7963cdbb66f328e8a3a5fb4a688894e9ab808593
aggre <hiroyuki.aggre@gmail.com>
2023-11-10 14:57:50 +0900

    typing improvements

--- src/ui/components/Checkout/types.ts
+++ src/ui/components/Checkout/types.ts
@@ -3,7 +3,10 @@ import type { CurrencyOption } from '../../../constants/currencyOption'
 export type CheckoutOptions = {
 	readonly amount: number
 	readonly propertyAddress: string
-	readonly currency?: CurrencyOption
+	readonly currency?:
+		| CurrencyOption
+		| Uppercase<CurrencyOption>
+		| Lowercase<CurrencyOption>
 	readonly fiatCurrency?: string
 	readonly rpcUrl: string
 	readonly feeBeneficiary?: string
