f634bdbe237c17622b7d560f7fc4eb6eeb670e17
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2023-11-15 12:18:29 +0900

    removes unused code

--- src/ui/components/Checkout/i18n/index.ts
+++ src/ui/components/Checkout/i18n/index.ts
@@ -53,28 +53,4 @@ export const Strings = {
 		en: ([amount]) => `${amount} DEV will be staked automatically.`,
 		ja: ([amount]) => ``,
 	},
-	// ModalCloseTicketConfirm: {
-	// 	en: 'Close',
-	// 	ja: '閉じる',
-	// },
-	// ModalActionTicketConfirm: {
-	// 	en: 'Yes',
-	// 	ja: 'はい',
-	// },
-	// ModalMessageNotConnected: {
-	// 	en: 'Please connect a wallet first',
-	// 	ja: 'ウォレットに接続してください',
-	// },
-	// ModalCloseNotConnected: {
-	// 	en: 'OK',
-	// 	ja: 'わかりました',
-	// },
-	// Expiration: {
-	// 	en: ([time]) => `Expiration date is ${time}.`,
-	// 	ja: ([time]) => `有効期限: ${time}`,
-	// },
-	// ModalMessageNotSigned: {
-	// 	en: `Can't sign with your wallet? Close this confirmation, and disconnect/reconnect your wallet and try again.`,
-	// 	ja: 'ウォレットで署名ができませんか? この確認を閉じてからウォレットを再接続してもう一度試してください。',
-	// },
 } satisfies ClubsI18nParts
