f7d4b786572cd8d7f3680c3a758742c0c9b93eb3
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2024-02-05 00:01:35 +0530

    add: isAdmin function

--- src/authenticate.ts
+++ src/authenticate.ts
@@ -47,3 +47,36 @@ export const authenticate = async ({
 
 	return userShare >= previousConfig.adminRolePoints
 }
+
+export const isAdmin = async ({
+	address,
+	previousConfiguration,
+	provider,
+}: {
+	readonly address: string
+	readonly previousConfiguration: string
+	readonly provider: AbstractProvider | ContractRunner
+}) => {
+	const previousConfig = decode(previousConfiguration)
+
+	const propertyClient = await clientsProperty(
+		provider,
+		previousConfig.propertyAddress
+	)
+
+	// eslint-disable-next-line functional/no-conditional-statement
+	if (!propertyClient || propertyClient.length <= 0) {
+		return false
+	}
+
+	const property = propertyClient[0] ?? propertyClient[1]
+	const totalSupply = BigInt((await property?.totalSupply()) ?? 0)
+	const userBalance = BigInt((await property?.balanceOf(address)) ?? 0)
+
+	const userShare = Number(
+		// Parsing (balance * 1e18 / supply) with 1e16
+		formatUnits((userBalance * WeiPerEther) / totalSupply, 16)
+	)
+
+	return userShare >= previousConfig.adminRolePoints
+}
