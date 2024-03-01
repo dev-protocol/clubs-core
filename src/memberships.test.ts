9eb16d4268065965c81bb41fab28de19cbc17b12
aggre <hiroyuki.aggre@gmail.com>
2024-03-01 19:08:51 +0900

    remove unused imports

--- src/memberships.test.ts
+++ src/memberships.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi, type Mock } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { membershipVerifierFactory } from './memberships'
 import { JsonRpcProvider, ZeroAddress, randomBytes } from 'ethers'
 import { Membership } from './types'
