087bc69fb4a18e4ed21c70813c70460ceedf87b2
Aggre <hiroyuki.aggre@gmail.com>
2024-07-31 09:52:34 +0900

    change `planned-structure` to `.preview`

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,6 @@ module.exports = {
 	presets: [clubs],
   mode: 'jit',
   content: [
-    './{planned-structure,src,example}/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
+    './{.preview,src,example}/**/*.{astro,js,jsx,svelte,ts,tsx,vue}',
   ],
 }
