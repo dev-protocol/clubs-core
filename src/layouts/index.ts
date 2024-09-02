52e2515cbdfa35db150a798c23dfa5abf1e8e2f9
Aggre <hiroyuki.aggre@gmail.com>
2024-09-02 14:46:11 +0900

    add pictogram

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,6 +1,9 @@
+import ClubsPictogram from './icons/clubs-pictogram.svg'
+
 export { default as Admin } from './Admin.astro'
 export { default as AdminConfigurationManager } from './AdminConfigurationManager.astro'
 export { default as Aside } from './Aside.astro'
 export { default as Modal } from './Modal.astro'
 export { default as CreateNavigationLink } from './CreateNavigationLink.astro'
 export { default as Clubs } from './primitives/Clubs.astro'
+export { ClubsPictogram }
