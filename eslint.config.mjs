e6f1a5b9ec2748d974c074045fe6e8c0053458ff
aggre <hiroyuki.aggre@gmail.com>
2024-10-12 18:51:53 +0900

    ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,5 +32,5 @@ export default tseslint.config(
 			'@typescript-eslint/no-explicit-any': 'off',
 		},
 	},
-	{ ignores: ['dist', 'ui', 'example', '*.{ts,mjs,js}'] }
+	{ ignores: ['dist', 'ui', 'example', '.yarn', '*.{ts,mjs,js}'] }
 )
