5f547bd4e08a720dcc1bb41104b5ae4ec2ce3f0b
aggre <hiroyuki.aggre@gmail.com>
2024-12-19 18:31:40 +0900

    Improve modal behavior (#1871)
    * fix modal behavior
    
    * 3.22.5

--- src/ui/components/Modal.vue
+++ src/ui/components/Modal.vue
@@ -1,68 +1,26 @@
 <script setup lang="ts">
-import { Component as VueComponent } from 'vue'
+import { useTemplateRef, Component as VueComponent, watch } from 'vue'
 
-defineProps<{
+const props = defineProps<{
 	eoa?: string
 	modalClose?: () => void
 	isVisible: boolean
 	modalContent: VueComponent
 	attrs: { [key: string]: any }
 }>()
+
+const dialog = useTemplateRef('dialog')
+
+watch(props, () => {
+	if (props.isVisible) {
+		dialog.value?.showModal()
+	} else {
+		dialog.value?.close()
+	}
+})
 </script>
 
 <style>
-.modal-container {
-	position: fixed;
-	top: 0;
-	right: 0;
-	bottom: 0;
-	left: 0;
-	display: flex;
-	justify-content: center;
-	align-items: flex-end;
-	z-index: 10;
-}
-
-@media (min-width: 1024px) {
-	.modal-container {
-		align-items: center;
-	}
-}
-
-.modal-overlay {
-	position: fixed;
-	top: 0;
-	right: 0;
-	bottom: 0;
-	left: 0;
-	background: rgba(0, 0, 0, 0.6);
-	z-index: -1;
-}
-
-.modal-content {
-	display: flex;
-	flex-direction: column;
-	padding: 1rem;
-	width: 100%;
-	max-width: 64rem;
-	background: white;
-	border-top-left-radius: 0.5rem;
-	border-top-right-radius: 0.5rem;
-	border-bottom-left-radius: 0;
-	border-bottom-right-radius: 0;
-	box-shadow: 0 0 1rem rgba(0, 0, 0, 0.2);
-
-	max-height: 80vh;
-	overflow-y: auto;
-}
-
-@media (min-width: 1024px) {
-	.modal-content {
-		border-bottom-left-radius: 0.5rem;
-		border-bottom-right-radius: 0.5rem;
-	}
-}
-
 .v-enter-active {
 	transition: transform 600ms cubic-bezier(0.07, 1.28, 0.5, 1);
 }
@@ -79,27 +37,25 @@ defineProps<{
 	transform: translate(0, 0);
 }
 
-html:has(#modal-container[data-active='true']) {
+html:has(dialog[open]) {
 	overflow: hidden;
 }
 </style>
 
 <template>
 	<div>
-		<Teleport to="body">
-			<div
-				id="modal-container"
-				v-show="isVisible"
-				class="modal-container"
-				:data-active="isVisible"
-			>
-				<div class="modal-overlay"></div>
-				<Transition>
+		<dialog
+			ref="dialog"
+			class="fixed inset-0 flex items-center justify-center overflow-y-auto"
+			:class="{ hidden: !isVisible }"
+		>
+			<div class="fixed inset-0 bg-black/60"></div>
+			<Transition>
+				<div class="relative m-auto w-full py-4">
 					<component
 						:eoa="eoa"
 						:modalClose="modalClose"
 						v-show="isVisible"
-						class="modal-content"
 						:is="modalContent"
 						v-bind="attrs"
 					>
@@ -107,8 +63,8 @@ html:has(#modal-container[data-active='true']) {
 							<slot name="after:description" />
 						</template>
 					</component>
-				</Transition>
-			</div>
-		</Teleport>
+				</div>
+			</Transition>
+		</dialog>
 	</div>
 </template>
