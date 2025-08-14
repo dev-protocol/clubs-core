d59110ade81873b1a929a34c86d9f2ce1f6bbec0
Aggre <hiroyuki.aggre@gmail.com>
2025-08-14 12:16:55 +0900

    Adds utility to escape special Redis tag characters
    Introduces a helper to properly escape reserved characters in Redis tags,
    improving query reliability and preventing parsing errors during tag searches.
    
    References official Redis documentation for escaping conventions.

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -40,3 +40,7 @@ export const Redis = (() => {
 		},
 	}
 })()
+
+// reference: https://redis.io/docs/latest/develop/interact/search-and-query/advanced-concepts/escaping/
+export const escapeTag = (tag?: string) =>
+	tag?.replace(/[,.<>{}[]"':;!@#$%^&*()-+=~]/g, '\\$&')
